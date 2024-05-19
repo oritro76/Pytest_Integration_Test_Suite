d8f87fc122c7b87e8b75e44c8ad6c7587ee76ae6
oritro76 <aritra.banerjee207@gmail.com>
2024-05-19 13:41:38 +0200

    add device specific settings

---
+++ tests/utils/utils.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+from itertools import product
+from loguru import logger
+# from dotenv import find_dotenv, load_dotenv
+
+
+def log_request(response: Any, *args: Any, **kwargs: Any) -> None:
+    """
+    Logs the request URL, headers, and body.
+
+    Args:
+        response: The response object containing request information.
+        *args: Additional arguments (unused in this function).
+        **kwargs: Additional keyword arguments (unused in this function).
+    """
+
+    logger.debug(f"Request: {response.request.method} {response.request.url}")
+    logger.debug(f"Headers: {response.request.headers}")
+    logger.debug(f"Body: {response.request.body}")
+
+
+def log_response(response: Any, *args: Any, **kwargs: Any) -> None:
+    """
+    Logs the response status code, headers, and body.
+
+    Args:
+        response: The response object containing response information.
+        *args: Additional arguments (unused in this function).
+        **kwargs: Additional keyword arguments (unused in this function).
+    """
+
+    logger.debug(f"Response Status Code: {response.status_code}")
+    logger.debug(f"Response Headers: {response.headers}")
+    logger.debug(f"Response Body: {response.text}")
+
+def generate_combinations(tuple_list):
+    # Use itertools.product to generate the Cartesian product of the tuples
+    return list(product(*tuple_list))
+
