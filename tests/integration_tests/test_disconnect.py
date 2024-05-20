744759b114b19098827db211d8ebd6bf1bd9f7a4
oritro76 <aritra.banerjee207@gmail.com>
2024-05-20 12:13:27 +0200

    add one more test case

--- tests/integration_tests/test_disconnect.py
+++ tests/integration_tests/test_disconnect.py
@@ -15,3 +15,8 @@ def test_disconnect(client):
     validate_response(model=GenericIsSuccessResponse, response=response)
 
     assert_response_success_status(response)
+
+def test_disconnect_with_invalid_body(client, random_data):
+    client.connect_to_a_random_device()
+    response = client.disconnect_device(data=random_data.random_json())
+    assert response.status_code == 400
